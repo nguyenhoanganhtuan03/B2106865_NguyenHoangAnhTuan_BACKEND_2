009d28151c0193e761bd0f2e2f23f64c269f51f0
Nguyễn Hoàng Anh Tuấn <127226234+nguyenhoanganhtuan03@users.noreply.github.com>
2025-02-16 21:48:19 +0700

    Cai dat ket noi MongoDB

---
+++ app/services/contact.service.js
@@ -0,0 +1,10 @@
+const { ObjectId }= require("mongodb");
+
+class ContactService {
+    constructor(client) {
+        this.Contact = client.db().collection("contacts");
+    }
+
+}
+
+module.exports = ContactService;
